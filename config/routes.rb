df40cfa08b61051496c30ba42dd7da97d425bc58
pazuru25 <neko.pazuru25@gmail.com>
2023-07-28 17:25:03 +0900

    JavaScript点滅機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   root to: "homes#index"
   resources :plans
   get '/todo', to: 'plans#todo'
-  get '/audio/Countdown03-1.mp3', to: 'audio#countdown_sound', format: 'mp3'
   get '/today_calendar/:date', to: 'plans#daily'
 end
