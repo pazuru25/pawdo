23479b3e31ab7b8bb2e22962144436bf9be4456a
pazuru25 <neko.pazuru25@gmail.com>
2023-08-03 17:42:18 +0900

    通知機能のバリデーション削除

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -7,6 +7,4 @@ class Plan < ApplicationRecord
   validates :schedule_date,      presence: true
   validates :schedule_time,      presence: true
   validates :color_id,           presence: true
-  validates :repetition_id,      presence: true
-  validates :recurring_id,       presence: true
 end
