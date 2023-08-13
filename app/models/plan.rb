5c43af0a3502fdaa6299e8e78b8827729e96715e
pazuru25 <neko.pazuru25@gmail.com>
2023-08-14 01:05:28 +0900

    TODO時間修正

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,6 +2,7 @@ class Plan < ApplicationRecord
   serialize :notification, Array
   belongs_to :user
 
+  self.default_timezone = :local # or 'Asia/Tokyo'
 
   validates :title,              presence: true
   validates :schedule_date,      presence: true
