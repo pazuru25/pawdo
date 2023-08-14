70bacfb7929258dd5a9d4aacbcfdf849c5823f0d
pazuru25 <neko.pazuru25@gmail.com>
2023-08-14 12:04:08 +0900

    タイムゾーン修正

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Pawdo
     config.i18n.default_locale = :ja
     config.beginning_of_week = :sunday
     config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
 
     
     # Configuration for the application, engines, and railties goes here.
